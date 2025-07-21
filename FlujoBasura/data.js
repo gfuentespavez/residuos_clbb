a2b340c63e1923dd98ab0393d3e1874f11e720cf
gfuentespavez <160777049+gfuentespavez@users.noreply.github.com>
2025-07-21 00:48:55 -0400

    GPS for comunas, rellenos sanitarios, and connections

---
+++ FlujoBasura/data.js
@@ -0,0 +1,88 @@
+export const garbageData = [
+    // Relleno Cemarc Penco
+    { comuna: "Penco", relleno: "Relleno Cemarc Penco", toneladas: 50 },
+    { comuna: "Concepción", relleno: "Relleno Cemarc Penco", toneladas: 50 },
+    { comuna: "Tomé", relleno: "Relleno Cemarc Penco", toneladas: 50 },
+    { comuna: "San Pedro de la Paz", relleno: "Relleno Cemarc Penco", toneladas: 50 },
+    { comuna: "Coronel", relleno: "Relleno Cemarc Penco", toneladas: 50 },
+    { comuna: "Lota", relleno: "Relleno Cemarc Penco", toneladas: 50 },
+    { comuna: "Chiguayante", relleno: "Relleno Cemarc Penco", toneladas: 50 },
+    { comuna: "Hualqui", relleno: "Relleno Cemarc Penco", toneladas: 50 },
+    { comuna: "Lebu", relleno: "Relleno Cemarc Penco", toneladas: 50 },
+    { comuna: "Santa Juana", relleno: "Relleno Cemarc Penco", toneladas: 50 },
+    { comuna: "Los Álamos", relleno: "Relleno Cemarc Penco", toneladas: 50 },
+
+    //Relleno Los Ángeles
+    { comuna: "Los Ángeles", relleno: "Relleno Los Ángeles", toneladas: 301},
+    { comuna: "Santa Bárbara", relleno: "Relleno Los Ángeles", toneladas: 301},
+    { comuna: "Alto Biobío", relleno: "Relleno Los Ángeles", toneladas: 102},
+    { comuna: "Nacimiento", relleno: "Relleno Los Ángeles", toneladas: 120 },
+    { comuna: "Tirúa", relleno: "Relleno Los Ángeles", toneladas: 100 },
+    { comuna: "Contulmo", relleno: "Relleno Los Ángeles", toneladas: 50 },
+    { comuna: "Cañete", relleno: "Relleno Los Ángeles", toneladas: 100 },
+    { comuna: "Quilleco", relleno: "Relleno Los Ángeles", toneladas: 100},
+    { comuna: "Tucapel", relleno: "Relleno Los Ángeles", toneladas: 100},
+    { comuna: "Laja", relleno: "Relleno Los Ángeles", toneladas: 20},
+    { comuna: "Yumbel", relleno: "Relleno Los Ángeles", toneladas: 100},
+    { comuna: "Cabrero", relleno: "Relleno Los Ángeles", toneladas: 200},
+
+    //Relleno Las Cruces
+    { comuna: "Talcahuano", relleno: "Relleno Fundo Las Cruces", toneladas: 350 },
+    { comuna: "Antuco", relleno: "Relleno Fundo Las Cruces", toneladas: 40},
+    { comuna: "Hualpén", relleno: "Relleno Fundo Las Cruces", toneladas: 120 },
+    { comuna: "Florida", relleno: "Relleno Fundo Las Cruces", toneladas: 150},
+
+    //Vertedero Licura Mulchén
+    { comuna: "San Rosendo", relleno: "Vertedero Licura", toneladas: 20},
+    { comuna: "Quilaco", relleno: "Vertedero Licura", toneladas: 150 },
+    { comuna: "Negrete", relleno: "Vertedero Licura", toneladas: 150 },
+    { comuna: "Mulchén", relleno: "Vertedero Licura", toneladas: 150 },
+
+    //Relleno Arauco - Chue
+    { comuna: "Arauco", relleno: "Relleno Sanitario Arauco Curanilahue", toneladas: 200},
+    { comuna: "Curanilahue", relleno: "Vertedero Licura", toneladas: 200},
+];
+
+export const comunaCoords = {
+    "Alto Biobío": [-71.3331, -37.9815],
+    "Antuco": [-71.6628, -37.3353],
+    "Arauco": [-73.3142, -37.2439],
+    "Cabrero": [-72.4036, -37.0294],
+    "Cañete": [-73.3957, -37.8003],
+    "Chiguayante": [-73.0161, -36.9158],
+    "Concepción": [-73.0503, -36.8270],
+    "Contulmo": [-73.2384, -38.0163],
+    "Coronel": [-73.1374, -37.0177],
+    "Curanilahue": [-73.3411, -37.4764],
+    "Florida": [-72.6642, -36.8219],
+    "Hualpén": [-73.1192, -36.8094],
+    "Hualqui": [-72.9378, -36.9782],
+    "Laja": [-72.7128, -37.2842],
+    "Lebu": [-73.6558, -37.6096],
+    "Los Álamos": [-73.4542, -37.6226],
+    "Los Ángeles": [-72.3537, -37.4693],
+    "Lota": [-73.1593, -37.0911],
+    "Mulchén": [-72.2415, -37.7183],
+    "Nacimiento": [-72.6738, -37.5058],
+    "Negrete": [-72.5401, -37.6015],
+    "Penco": [-72.9926, -36.7410],
+    "Quilaco": [-72.018394, -37.6865092],
+    "Quilleco": [-71.9702, -37.4709],
+    "San Pedro de la Paz": [-73.1070, -36.8524],
+    "San Rosendo": [-72.7184, -37.2662],
+    "Santa Bárbara": [-72.0293062, -37.6651548],
+    "Santa Juana": [-72.9463, -37.1751],
+    "Talcahuano": [-73.1154, -36.7249],
+    "Tirúa": [-73.4913, -38.3377],
+    "Tomé": [-72.9555, -36.6190],
+    "Tucapel": [-72.1678, -37.2858],
+    "Yumbel": [-72.5762, -37.0984]
+};
+
+export const rellenoCoords = {
+    "Relleno Cemarc Penco": [-72.9833261, -36.7634115],
+    "Relleno Sanitario Arauco Curanilahue": [-73.364, -37.409],
+    "Relleno Los Ángeles": [-72.3395474, -37.2998048],
+    "Vertedero Licura": [-72.2754906, -37.654802],
+    "Relleno Fundo Las Cruces": [-72.1686415, -36.6967439],
+};
